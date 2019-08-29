1061025ad8bcc990eab051eff4ac81d7cbf795ae
Sinas66 <0638175303@ukr.net>
2019-08-29 19:01:39 +0300

    update initial

---
+++ build/precache-manifest.fbd9cee41d2775995282d1af2ec5a31a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5811cb1d233d221466edfca198c268f",
+    "url": "/CurriculumVitae/index.html"
+  },
+  {
+    "revision": "aa7eca6308a31efaf356",
+    "url": "/CurriculumVitae/static/css/main.aeb2fd80.chunk.css"
+  },
+  {
+    "revision": "2417972f131251f18173",
+    "url": "/CurriculumVitae/static/js/2.720da36f.chunk.js"
+  },
+  {
+    "revision": "aa7eca6308a31efaf356",
+    "url": "/CurriculumVitae/static/js/main.6830ce44.chunk.js"
+  },
+  {
+    "revision": "1a1cd3871d8e7d41e3ba",
+    "url": "/CurriculumVitae/static/js/runtime~main.2474b98e.js"
+  },
+  {
+    "revision": "16a3218a723fa469b7b4731bbdc7acdd",
+    "url": "/CurriculumVitae/static/media/JS_Logo.16a3218a.svg"
+  }
+]);
